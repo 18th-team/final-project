089bf04b97aedf0ffb36898e404450c975484f1c
yeeun <jovm268@naver.com>
2025-03-18 17:42:37 +0900

    메인 FE작업

--- src/main/java/com/team/controller/HomeController.java
+++ src/main/java/com/team/controller/HomeController.java
@@ -1,15 +1,14 @@
 package com.team.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String home() {
-        return "index";
-    }
 
-    @GetMapping("/mobti")
-    public String test() { return "mobti_test"; }
-}
+    @GetMapping("/")
+    public String home(Model model) {
+    return "index";
+}}
