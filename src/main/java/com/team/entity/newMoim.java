9a57bfeab90987deb23d499a4414d6e0d569e022
yeeun <jovm268@naver.com>
2025-03-19 12:09:20 +0900

    newMoim entity생성

--- src/main/java/com/team/entity/newMoim.java
+++ src/main/java/com/team/entity/newMoim.java
@@ -4,7 +4,10 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
