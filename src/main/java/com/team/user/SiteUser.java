6ac35c7d4fa5ec6434faebe55afe8bffb1601ae5
jsw <sss027392@gmail.com>
2025-03-19 18:03:34 +0900

    유저 엔티티

--- src/main/java/com/team/user/SiteUser.java
+++ src/main/java/com/team/user/SiteUser.java
@@ -1,4 +1,36 @@
 package com.team.user;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
 public class SiteUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column(nullable = false)
+    private String age;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(unique = true)
+    private String phone;
+
+    @Column
+    private String profileImage;
+
+    @Column(nullable = false)
+    private LocalDate createdAt = LocalDate.now();
 }
