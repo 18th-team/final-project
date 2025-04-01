65af5655bea266307199dda9701700f3f8a24f19
yeeun <jovm268@naver.com>
2025-04-01 17:50:06 +0900

    카테고리 비회원 접근 가능

--- src/main/java/com/team/moim/repository/ClubRepository.java
+++ src/main/java/com/team/moim/repository/ClubRepository.java
@@ -12,6 +12,13 @@ import java.util.Set;
 
 @Repository
 public interface ClubRepository extends JpaRepository<Club, Long> {
+
+    // ✅ 특정 카테고리에 속하는 클럽 조회 (categoryId 기준)
+    List<Club> findByKeywords_Id(Long keywordId);
+
+
+
+
     // Keyword의 name 값을 기준으로 Club 검색
     List<Club> findByKeywords_NameIn(List<String> selectedThemes);
 
@@ -19,5 +26,4 @@ public interface ClubRepository extends JpaRepository<Club, Long> {
     @Query("SELECT c FROM Club c JOIN c.keywords k WHERE k.name IN :keywords GROUP BY c HAVING COUNT(k) > 0")
     List<Club> findByKeywords(@Param("keywords") List<String> keywords);
 
-    List<Club> findByKeywords_Name(String keywordName); // 특정 키워드를 가진 클럽 조회
 }
